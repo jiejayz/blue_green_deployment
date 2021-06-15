002c5aacdaac904415d7277716aa02d819dc3efc
jiejayz <82641662+jiejayz@users.noreply.github.com>
2021-06-15 00:05:55 -0700

    Updated to 01-prod-uction-blue

--- BLUE/bootstrap.tf
+++ BLUE/bootstrap.tf
@@ -4,7 +4,7 @@ variable "infrastructure_version" {
 
 terraform {
   backend "s3" {
-    bucket = "01-prod-blue"
+    bucket = "01-prod-uction-blue"
     region = "us-west-1"
     key    = "terraform.tfstate"
   }
